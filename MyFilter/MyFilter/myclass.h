470e18b91da1759b99c4ff7eeb07509f84c0f6b1
Margo63 <margo-vinograd@mail.ru>
2023-01-31 22:03:47 +0300

    Add files via upload

---
+++ MyFilter/MyFilter/myclass.h
@@ -0,0 +1,39 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+#include <QObject>
+
+
+struct Products{
+    int price;
+    QString product;
+
+};
+
+class MyClass:public QObject
+{
+    Q_OBJECT
+
+
+public:
+    explicit MyClass(QObject* parent = nullptr);
+
+    Q_INVOKABLE void add(QString price,QString product);
+    Q_INVOKABLE void remove(QString price,QString product);
+    Q_INVOKABLE void show();
+
+    Q_INVOKABLE int getPrice(int index);
+    Q_INVOKABLE QString getProduct(int index);
+
+
+    Q_INVOKABLE void priceSort();
+
+private:
+
+    std::vector<Products> list_products;
+
+};
+
+
+
+#endif // MYCLASS_H
