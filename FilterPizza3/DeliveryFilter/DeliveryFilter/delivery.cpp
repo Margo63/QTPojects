8b94dfe74fbbf5be2fc9a252188cf5fe8bcacde1
Margo63 <margo-vinograd@mail.ru>
2023-02-11 19:18:54 +0300

    Add files via upload

---
+++ FilterPizza3/DeliveryFilter/DeliveryFilter/delivery.cpp
@@ -0,0 +1,83 @@
+#include "delivery.h"
+#include <QDebug>
+Delivery::Delivery(QObject*parent):QObject(parent){}
+
+void Delivery::filtration(int filter_value, int filter_type)//передаем тип по которому будем фильтровать(по цене, региону доставки или по типу(острая и тд))
+{
+
+    filtered_list.resize(0);//обнуляем отфильтрованный список
+    switch(filter_type){
+        case PRICE:{
+            for(size_t i=0;i<pizzas.size();i++){//просматриваем все елементы вектра всех пицц
+                if(pizzas[i].price<=filter_value)//если фильтруем по цене, то в итоговый вектор добавляем если цена пиццы меньше фильтровочной цены
+                    filtered_list.push_back(pizzas[i]);//добавляем пиццу
+            }
+            break;
+        }
+        case TYPE:{
+            for(size_t i=0;i<pizzas.size();i++){//просматриваем все елементы вектра всех пицц
+                if(pizzas[i].type==filter_value)//если фильтруем по типу, то в итоговый вектор добавляем если тип рассматриваемой пиццы и переданный тип совпадают
+                    filtered_list.push_back(pizzas[i]);//добавляем пиццу
+            }
+            break;
+        }
+        case REGION:{
+            for(size_t i=0;i<pizzas.size();i++){//просматриваем все елементы вектра всех пицц
+                if(pizzas[i].delivery_region==filter_value)//если фильтруем по региону, то в итоговый вектор добавляем если регион рассматриваемой пиццы и переданный регион совпадают
+                    filtered_list.push_back(pizzas[i]);//добавляем пиццу
+            }
+
+            break;
+        }
+
+    }
+}
+
+int Delivery::pizzaCount()//геттер для количества всех пицц
+{
+    return pizzas.size();
+}
+
+int Delivery::filterCount()//геттер для количества отфильтрованных пицц
+{
+    return filtered_list.size();
+}
+
+
+int Delivery::getPrice(int index,int list)//геттер для цены, чтобы не писать дублирующие функции передаем еще и индекс вектора(0-pizzas 1-filtered_list)
+{
+    if(list==PIZZAS)
+        return pizzas[index].price;
+    if(list==FILTERED)
+        return filtered_list[index].price;
+    return -1;
+}
+
+int Delivery::getType(int index,int list)//геттер для типа
+{
+    if(list==PIZZAS)
+        return pizzas[index].type;
+    if(list==FILTERED)
+        return filtered_list[index].type;
+    return -1;
+}
+
+int Delivery::getRegion(int index,int list)//геттер для региона
+{
+    if(list==PIZZAS)
+        return pizzas[index].delivery_region;
+    if(list==FILTERED)
+        return filtered_list[index].delivery_region;
+    return -1;
+}
+
+QString Delivery::getName(int index,int list)//геттер для названия пиццы
+{
+    if(list==PIZZAS)
+        return pizzas[index].name;
+    if(list==FILTERED)
+        return filtered_list[index].name;
+    return "";
+}
+
+
